faaf995b0f1717ce75db29acc3e0486197cadfbf
bharath <33729709+bharath5673@users.noreply.github.com>
2022-11-03 11:22:09 +0530

    DeepStream-Yolo-onnx

--- DeepStream-Yolo-onnx/build.sh
+++ DeepStream-Yolo-onnx/build.sh
@@ -3,8 +3,8 @@
 echo "\n\n[INFO] : This build works for Yolov5 and Yolov7 \n"
 
 echo "\n[INFO] : please wait... \n"
-make -C nvdsinfer_custom_impl_Yolo clean
+make $1 -C nvdsinfer_custom_impl_Yolo clean
 echo "\n[INFO] : clean complete... \n"
-CUDA_VER=11.6 make -C nvdsinfer_custom_impl_Yolo
+make $1  -C nvdsinfer_custom_impl_Yolo
 
 echo "\n\n[INFO] : build complete.. \n\n"
